6c2eb232e3df9aa5252c15a99bbf02bd204ade69
tonynater <tong.t.wang@gmail.com>
2016-02-20 16:26:46 -0800

    Server using express

--- server.js
+++ server.js
@@ -1,110 +1,70 @@
-/*
-Copyright (c) 2013, Eelco Cramer
-All rights reserved.
+//License: https://github.com/Chillee/AudioStream/blob/master/LICENSE.md
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
 
-* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-* Neither the name of 'The Netherlands Organisation for Applied Scientific Research TNO' nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-var socketIO = require('socket.io');
-var uuid = require('node-uuid');
-var static = require('node-static');
-var os = require('os');
-
-//
-// Create a node-static server instance to serve the './public' folder
-//
-
-var file = new(static.Server)('./client');
-
-var server = require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-});
-
-// Create and configure socket.io 
-//
-var io = socketIO.listen(server, {log: false});
-
-// configuration needed for Heroku hosting
-if (os.platform() !== 'win32') {
-    io.configure(function() {
-        io.set("transports", ["xhr-polling"]);
-        io.set("polling duration", 10);
-    });
-}
-
-var port = process.env.PORT || 8080;
-
-server.listen(port);
-console.log('Listening on port: ' + port);
-
-// keeping track of connections
-var sockets = {};
+app.use('/', express.static('static/public'));
 
 function makeid(){
+    var possible = "abcdefghijklmnopqrstuvwxyz";
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for( var i=0; i < 5; i++ )
+    for (var i = 0; i < 5; i++) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
     return text;
 }
 
-io.sockets.on('connection', function(socket) {
-	var id;
+var sockets = {};
+io.on('connection', function(socket) {
+  var id;
+  do {
+    id = makeid();
+  } while(sockets[id]);
 
-	// determine an identifier that is unique for us.
+  sockets[id] = socket;
+  socket.emit('your-id', id);
 
-	do {
-		id = makeid();
-	} while (sockets[id]);
+  socket.on('disconnect', function() {
+    sockets[socket] = undefined;
+    delete sockets[socket];
+  });
 
-	// we have a unique identifier that can be sent to the client
+  socket.on('message', function(message) {
+    if (sockets[message.to]) {
+      sockets[message.to].emit('message', message);
+    } else {
+      socket.emit('disconnected', message.from);
+    }
+  });
 
-	sockets[id] = socket;
-	socket.emit('your-id', id);
+  socket.on('logon', function(message) {
+    if (sockets[message.to]) {
+      sockets[message.to].emit('logon', message);
+    } else {
+      socket.emit('error', 'Does not exsist at server.');
+    }
+  });
 
-	// remove references to the disconnected socket
-	socket.on('disconnect', function() {
-		sockets[socket] = undefined;
-		delete sockets[socket];
-	});
-
-	// when a message is received forward it to the addressee
-	socket.on('message', function(message) {
-		if (sockets[message.to]) {
-			sockets[message.to].emit('message', message);
-		} else {
-			socket.emit('disconnected', message.from);
-		}
-	});
-
-	// when a listener logs on let the media streaming know about it
-	socket.on('logon', function(message) {
-		if (sockets[message.to]) {
-			sockets[message.to].emit('logon', message);
-		} else {
-			socket.emit('error', 'Does not exsist at server.');
-		}
-	});
-
-	socket.on('logoff', function(message) {
-		if (sockets[message.to]) {
-			sockets[message.to].emit('logoff', message);
-		} else {
-			socket.emit('error', 'Does not exsist at server.');
-		}
-	});
+  socket.on('logoff', function(message) {
+    if (sockets[message.to]) {
+      sockets[message.to].emit('logoff', message);
+    } else {
+      socket.emit('error', 'Does not exsist at server.');
+    }
+  });
 });
 
+app.get('/:room', function(req, res) {
+  if (!sockets[req.params.room]) {
+    res.writeHead(404, {'content-type': 'text/plain'});
+    res.end('Room not found');
+  } else {
+    res.sendFile('static/private/listen.html', {root: __dirname});
+  }
+});
 
+server.listen(8080, function(){
+  console.log('listening on *:8080');
+});
